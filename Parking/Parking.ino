9d3ce723bbffcb60cb722e5c940fa0d5df1a1ecc
Uri Shaked <uri@salsa4fun.co.il>
2017-07-23 21:26:58 +0300

    Add a small LCD display

--- Parking/Parking.ino
+++ Parking/Parking.ino
@@ -1,13 +1,17 @@
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
+#include <Adafruit_SSD1306.h>
+#include <U8x8lib.h>
 
 SoftwareSerial serialSIM800(D5, D6);
 
+U8X8_SSD1306_64X48_ER_HW_I2C u8x8(U8X8_PIN_NONE, D1, D2);
+
 #define APN "uinternet"
-#define TARGET_URL "parking-test.firebaseio.com/status.json"
+#define TARGET_URL "parking-sensor-app.firebaseio.com/s.json"
 
 const int trigPin = D4;
-const int echoPin = D2;
+const int echoPin = D7;
 
 StaticJsonBuffer<200> jsonBuffer;
 JsonObject& report = jsonBuffer.createObject();
@@ -43,6 +47,8 @@ bool readResponse(String expected = "OK", String ignore = "") {
         }
         if (line != expected) {
           Serial.println("!!! Unexpected Response");
+          u8x8.println("ERROR!");
+          u8x8.println(line);
           return false;
         } else {
           return true;
@@ -75,6 +81,11 @@ void setup() {
 
   Serial.begin(115200);
   Serial.println("Start");
+
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_r);
+  u8x8.println("Start!");
+
   serialSIM800.begin(9600);
   delay(3000);
   digitalWrite(LED_BUILTIN, HIGH);
@@ -92,6 +103,7 @@ void setup() {
   readResponse("OK", "ATE0");
 
   Serial.println("Modem ready, initializing GSM");
+  u8x8.println("Modem");
 
   do {
     sendCommand("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
@@ -118,9 +130,13 @@ long readDistance() {
 
 // the loop function runs over and over again forever
 void loop() {
+  u8x8.clearDisplay();
+  u8x8.home();
   Serial.println("Measuring...");
   long distance = readDistance();
   Serial.println(distance);
+  u8x8.print("D:");
+  u8x8.println(distance);
   sendCommand("AT+SAPBR=2,1");
   readResponse();
   sendCommand("AT+CGATT?");
@@ -152,8 +168,10 @@ void loop() {
   readResponse();
   sendCommand("AT+HTTPREAD");
   readResponse();
+  u8x8.println("Send");
   readResponse("+HTTPACTION");
   delay(100);
+  u8x8.println("Done");
   sendCommand("AT+HTTPTERM");
   readResponse();
   delay(10000);
